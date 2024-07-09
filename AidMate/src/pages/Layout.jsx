bbf7e6713a409601e5596037db9cd8a099b71283
Megan <megan.marson22@gmail.com>
2024-07-08 21:02:03 -0400

    Initial commit for routes

---
+++ AidMate/src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+import MainNav from "../components/MainNav";
+
+const Layout = () => {
+  return (
+    <>
+      <MainNav />
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
